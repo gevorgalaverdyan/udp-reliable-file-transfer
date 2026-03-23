6f2e611016d934703dbb43ce3f5de43ed04e9839
Gevorg Alaverdyan <57418717+gevorgalaverdyan@users.noreply.github.com>
2026-03-23 23:54:49 +0000

    argsparsing

--- client/client.py
+++ client/client.py
@@ -1,3 +1,6 @@
 class Client:
-    def __init__(self):
-        pass
+    def __init__(self, server_ip: str, port: int, filename: str, max_payload: int):
+        self.server_ip = server_ip
+        self.port = port
+        self.filename = filename
+        self.max_payload = max_payload
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import sys
 
+from client.client import Client
+
 def main():
-    print("This code runs when the file is executed directly.")
+    print("UDP program: \n")
 
     if len(sys.argv) > 1:
         print(f"Arguments passed: {sys.argv[1:]}")
+    
+
     return 0
 
+def parse_input(args: list[str]):
+    required_input = {
+        "server_ip": None,
+        "port": None,
+        "filename": None,
+        "max_payload": None,
+    }
+
+    if len(required_input!=len(args)):
+        print("Not enough parameters")
+        return
+
+    for arg in args:
+        if not required_input[arg]:
+            print(f"Extra input {arg} ignored")
+        else:
+            required_input[0]
+
+    # return Client()
+    
 if __name__ == "__main__":
     sys.exit(main())
