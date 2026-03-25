6640826b985bbb0213436a0989e153862da8232d
Gevorg Alaverdyan <57418717+gevorgalaverdyan@users.noreply.github.com>
2026-03-25 22:28:56 +0000

    client no loop

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 import random
 import socket
 import time
@@ -11,39 +12,63 @@ def log(msg: str):
 
 
 def rcv_file(server_ip: str, port: int, filename: str, segment_size: int):
+    path_to_output = Path("./files/received/"+filename)
+    path_to_output.parent.mkdir(parents=True, exist_ok=True)
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     conn_id = random.getrandbits(32)
     server_addr = (server_ip, port)
-    
+
     log(f"Connection ID: {conn_id}")
-    log(f"Requesting '{filename}' from {server_ip}:{port}  "
-        f"(segment_size={segment_size})")
-    
-    req_packet = pack_packet(conn_id, 0, MESSAGE_TYPES.REQUEST, False, filename.encode())
+    log(
+        f"Requesting '{filename}' from {server_ip}:{port}  "
+        f"(segment_size={segment_size})"
+    )
+
+    req_packet = pack_packet(
+        conn_id, 0, MESSAGE_TYPES.REQUEST, False, filename.encode()
+    )
     sock.sendto(req_packet, server_addr)
 
     raw_data, addr = sock.recvfrom(65535)
     packet = unpack_packet(raw_data)
-    
+
+    expected_seq = 0
+
     if not packet:
-        log("ERROR: received packet is NONE")
+        log("ERROR: malformed packet received")
         return
-    
+
     connection_id, sequence_number, message_type, is_final, payload = packet
     if connection_id != conn_id:
-        log(f"ERROR: connection ids not equal received={connection_id} expecting={conn_id}")
+        log(
+            f"ERROR: connection ids not equal received={connection_id} expecting={conn_id}"
+        )
         return
-    
-    if message_type==MESSAGE_TYPES.ERROR:
+
+    if message_type == MESSAGE_TYPES.ERROR:
         log(f"MESSAGE ERROR: {payload.decode('utf-8')}")
         return
-    elif message_type==MESSAGE_TYPES.DATA:
-        log(f"RECEIVED: seq={sequence_number}, payload_size={len(payload)}, is_final={is_final}")
-        return
-    else: 
+    elif message_type == MESSAGE_TYPES.DATA:
+        log(
+            f"RECEIVED: seq={sequence_number}, payload_size={len(payload)}, is_final={is_final}"
+        )
+        if sequence_number == expected_seq:
+            with open(path_to_output, "wb") as f:
+                f.write(payload)
+
+            ack_packet = pack_packet(conn_id, sequence_number, MESSAGE_TYPES.ACK, False, b"") 
+            sock.sendto(ack_packet, addr)
+            
+            if is_final:
+                return
+            
+            expected_seq ^= 1
+        else: 
+            log("ERROR: Unexpected seq number")
+    else:
         log(f"Unexpected packet type: {message_type.name}")
         return
-    
 
 
 def main():
@@ -53,7 +78,12 @@ def main():
     parser.add_argument("server_ip", help="Server IP address (e.g. 127.0.0.1)")
     parser.add_argument("port", type=int, help="Server UDP port")
     parser.add_argument("filename", help="Name of the file to retrieve from the server")
-    parser.add_argument("--segment-size", type=int, help=f"Max UDP payload bytes per DATA packet", required=True)
+    parser.add_argument(
+        "--segment-size",
+        type=int,
+        help=f"Max UDP payload bytes per DATA packet",
+        required=True,
+    )
 
     args = parser.parse_args()
     rcv_file(args.server_ip, args.port, args.filename, args.segment_size)
